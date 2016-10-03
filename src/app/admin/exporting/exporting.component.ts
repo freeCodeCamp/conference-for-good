0180017ded81c219e661c05d273729875ad3c8ba
Danny Libin <omegasol11@gmail.com>
2016-10-03 09:30:38 -0500

    checkboxs for all session fields, selective exports

--- src/app/admin/exporting/exporting.component.ts
+++ src/app/admin/exporting/exporting.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { Session } from '../../shared/session.model';
+import { SessionService } from '../../shared/session.service';
+import { TransitionService } from '../../shared/transition.service';
+import { ToastComponent } from '../../shared/toast.component';
 
 @Component({
   selector: 'app-exporting',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExportingComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('toast') toast: ToastComponent;
+
+  sessionFields: {name: string; checked: boolean}[] = [];
+
+  constructor(private transitionService: TransitionService,
+              private sessionService: SessionService) { }
 
   ngOnInit() {
+    this.transitionService.transition();
+    this.getFields();
+  }
+
+  getFields() {
+    let refSess = this.sessionService.sessionsActive.getValue()[0];
+    for (let field in refSess) {
+      if (refSess.hasOwnProperty(field)) {
+        if (field !== '_id' && field !== '__v') {
+          this.sessionFields.push({name: field, checked: false});
+        }
+      }
+    }
+  }
+
+  export() {
+    console.log(this.sessionFields);
   }
 
 }
